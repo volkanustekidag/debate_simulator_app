4eaa7a20ac16b26d093172061e306244050efe17
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2023-09-09 23:37:52 +0300

    Create home const file

---
+++ lib/features/home/presentation/const/home_const.dart
@@ -0,0 +1,49 @@
+class HomeConst {
+  static List<String> topicDescriptions = [
+    "The impact of technology on human connections.",
+    "The urgent need for environmental protection.",
+    "Ethical treatment of animals and hunting.",
+    "Should vaccines be mandatory?",
+    "Protecting personal data in the digital age.",
+    "Addressing wealth gaps and taxing the rich.",
+    "The future of VR and AR technologies.",
+    "Science vs. religious beliefs.",
+    "Dealing with immigration and refugees.",
+    "Promoting women's rights and gender parity.",
+    "Balancing censorship and free speech online.",
+    "Environmentally friendly food production.",
+    "Ethics of altering human genes.",
+    "Regulating firearms and their ownership.",
+    "AI's impact on jobs and society.",
+    "The role of faith in a secular world.",
+    "Coping with stress in education.",
+    "Shifting to renewable energy sources.",
+    "Ensuring healthcare for all and insurance systems."
+  ];
+  static List<String> discussionTopics = [
+    "Tech & Relationships",
+    "Climate Change",
+    "Animal Rights",
+    "Vaccination Mandates",
+    "Digital Privacy",
+    "Income Inequality",
+    "Virtual Reality",
+    "Evolution vs. Creationism",
+    "Immigration Policies",
+    "Gender Equality",
+    "Internet Censorship",
+    "Sustainable Food",
+    "Genetic Engineering",
+    "Gun Control",
+    "Artificial Intelligence",
+    "Religion & Secularism",
+    "Academic Pressure",
+    "Alternative Energy",
+    "Healthcare Access",
+  ];
+
+  static List<String> sides = [
+    "For",
+    "Against",
+  ];
+}
